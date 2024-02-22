09239feecdbc038a0c45f7bdd05be7a5fdec5aa4
ChiragSethi-1153 <chirag.1153@zenmonk.tech>
2024-02-22 07:09:33 +0530

    22/02/24 morning

--- src/services/newPost.service.js
+++ src/services/newPost.service.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const token = localStorage.getItem('token')
-console.log(token)
+// console.log(token)
 const config = {
         headers: {'authorization': `Bearer ${token}`},
         "Content-type": 'multipart/form-data'
--- src/services/postComment.service.js
+++ src/services/postComment.service.js
@@ -1,8 +1,14 @@
 import axios from "axios"
 
+const token = localStorage.getItem('token')
+console.log(token)
+const config = {
+        headers: {'authorization': `Bearer ${token}`},
+        "Content-type": 'application/json'
+}
 
 const postCommentService = async (inputs) => {
-    const response = await axios.post(`${process.env.REACT_APP_SERVER}/comment/${inputs}`)
+    const response = await axios.post(`${process.env.REACT_APP_SERVER}/comment`, inputs, config)
     return response
 }
 
